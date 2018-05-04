ab0dfe749be26af4fecfe080428bac984e23fc16
Sam Tay <sam.chong.tay@gmail.com>
2018-05-03 23:48:49 -0400

    Less finger exertion

--- keyboards/ergodox_ez/keymaps/samtay/keymap.c
+++ keyboards/ergodox_ez/keymaps/samtay/keymap.c
@@ -17,6 +17,12 @@ enum custom_keycodes {
   RGB_SLD
 };
 
+/*******************************************************************************
+  Graphical layout can be viewed at
+
+  http://www.keyboard-layout-editor.com/#/gists/e2831bdbee9113508428d49c9a963a06
+*******************************************************************************/
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
@@ -29,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MO(FRMW),   KC_TRNS, KC_TRNS,        KC_LCTL, ALT_T(KC_ESC),
                                                KC_BSLS,KC_TRNS,
                                                                 KC_TRNS,
-                                               KC_SPC, KC_MINS, KC_TRNS,
+                                               KC_SPC, KC_TRNS, KC_TRNS,
         // right hand
         KC_TRNS,  KC_6,   KC_7,  KC_8,   KC_9,   KC_0,    KC_END,
         KC_RPRN,  KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,    KC_QUOT,
@@ -38,24 +44,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   ALT_T(KC_ENT), KC_RCTL,KC_TRNS,KC_TRNS,KC_TRNS,
         KC_TRNS,  KC_DLR,
         KC_TRNS,
-        KC_TRNS,  KC_EQL, KC_BSPC
+        KC_TRNS,  KC_TRNS, KC_BSPC
     ),
 // SYMBOLS
 [SYMB] = LAYOUT_ergodox(
        // left hand
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
+       KC_EQL,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
        KC_TRNS,KC_AMPR,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_CAPS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
+       OSM(MOD_LSFT),KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
           KC_TRNS,KC_TRNS,KC_TRNS,KC_LCTL,ALT_T(KC_ESC),
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
                                KC_SPC,KC_MINS,KC_TRNS,
        // right hand
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_7,   KC_8,    KC_9,    KC_ASTR, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_7,   KC_8,    KC_9,    KC_ASTR, KC_MINS,
                 KC_PERC, KC_4,   KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-       KC_TRNS, KC_HASH, KC_1,   KC_2,    KC_3,    KC_BSLS, KC_CAPS,
+       KC_TRNS, KC_HASH, KC_1,   KC_2,    KC_3,    KC_BSLS, OSM(MOD_RSFT),
                          ALT_T(KC_ENT),KC_RCTL,  KC_0,    KC_EQL,  KC_TRNS,
        KC_TRNS,KC_TRNS,
        KC_TRNS,
